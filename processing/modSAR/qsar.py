ce622c7872e51f44831080e518ba44f815a32ac2
Jonathan Silva <jonathan.car.silva@gmail.com>
2018-10-29 18:26:12 -0200

    WIP: Exploring molecular descriptors. Handles #5

--- processing/modSAR/qsar.py
+++ processing/modSAR/qsar.py
@@ -18,12 +18,20 @@ from rdkit.DataStructs import cDataStructs
 class Dataset:
     """Generic class to represent a data set"""
 
-    def __init__(self, data, target):
+    def __init__(self, dataset_name, data, target):
+        self.dataset_name
         self.data = data
         self.target = target
         self.number_samples = data.shape[0]
         self.number_features = data.shape[1]
         self.feature_names = data.dtypes.index.tolist()
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        object_str = "Dataset %s \n -- Samples : %d\n -- Features:  %d"
+        return object_str % (self.dataset_name, self.number_samples, self.number_features)
 
 
 class QSARDataset(Dataset):
@@ -33,32 +41,24 @@ class QSARDataset(Dataset):
 
     """
 
-    def __init__(self, xls_file, dataset_name):
-        self.name = dataset_name
-        excelFile = pd.ExcelFile(xls_file)
+    def __init__(self, target_id, bioactivities_df,
+                 smiles_columns='canonical_smiles',
+                 activity_column='median_pchembl_value'):
 
-        self.metadata = excelFile.parse("metadata")
-        self.metadata.set_index("ID", inplace=True)
+        self.target_id = target_id
+        # self.metadata.set_index("ID", inplace=True)
 
-        self.features = excelFile.parse("features")
-        self.features.set_index(self.metadata["ID"])
+        self.features = descriptors
+        # self.features.set_index(self.metadata["ID"])
 
-        self.normalised_features = excelFile.parse("normalised_features")
-        self.normalised_features.set_index(self.metadata["ID"], inplace=True)
+        # self.normalised_features = excelFile.parse("normalised_features")
+        # self.normalised_features.set_index(self.metadata["ID"], inplace=True)
 
         # self.fingerprint = self._parse_fingerprint(excelFile.parse("ECFP4_bits").set_index(self.metadata["ID"])["BITS"])
-        super().__init__(self.normalised_features, self.metadata["NEW_ACTIVITY_VALUE"])
-
-    def _parse_fingerprint(self, ecfp4Bits):
-        activeBits = ecfp4Bits.apply(lambda bits: np.array(list(map(int, bits.split("|")))) - 1)
-        fpMatrix = sp.lil_matrix((activeBits.shape[0], 1024), dtype=np.int16)
-        for i in range(activeBits.shape[0]):
-            fpMatrix[i, activeBits[i]] = 1
-        fpDF = pd.DataFrame({'FINGERPRINT': np.apply_along_axis(lambda x: "".join(x.astype("str")), 1, fpMatrix.toarray())},
-                            index=self.metadata["ID"])
-        fpDF["FP_OBJ"] = [cDataStructs.CreateFromBitString(bits) for bits in fpDF["FINGERPRINT"]]
-        return fpDF
+        super().__init__(self.target_id, self.features, activity_value)
 
     def __str__(self):
-        object_str = "Dataset %s \n -- Samples : %d\n -- Features:  %d"
-        return object_str % (self.name, self.number_samples, self.number_features)
+        return self.__repr__()
+
+    def __repr__(self):
+        return 'QSAR' + super.__repr__()
