14152a631cdaa561658d1337f0d17ff21b37a440
Jon Cardoso <jon@datasciencebr.com>
2019-02-05 18:32:42 -0200

    Solve problems with Java Classpath

--- processing/modSAR/cdk_utils.py
+++ processing/modSAR/cdk_utils.py
@@ -32,14 +32,14 @@ class JavaCDKBridge:
         """Initialise paths"""
 
         if py4j_jar_path is None:
-            self.py4py4j_jar_path = py4j.java_gateway.find_jar_path()
+            self.py4j_jar_path = py4j.java_gateway.find_jar_path()
         else:
-            self.py4py4j_jar_path = py4j_jar_path
+            self.py4j_jar_path = py4j_jar_path
 
         if cdk_jar_path is None:
             jar_path = os.getenv('JAR_PATH')
             cdk_version = os.getenv('CDK_VERSION')
-            self.cdk_jar_path = "%s/cdk-%s" % (jar_path, cdk_version)
+            self.cdk_jar_path = "%s/cdk-%s.jar" % (jar_path, cdk_version)
         else:
             self.cdk_jar_path = cdk_jar_path
 
@@ -48,17 +48,21 @@ class JavaCDKBridge:
 
     def start_cdk_java_bridge(self):
         """Start a JVM bridge so we can access CDK capabilities from within Python"""
-
         with subprocess.Popen(['ps aux | grep CDK'], shell=True, stdout=subprocess.PIPE) as proc:
             line = proc.stdout.read()
-            print(line)
             if 'CDKBridge' in str(line):
                 print('CDK Bridge process running')
                 self.is_server_running = True
             else:
-                command_str = "java -cp '{}:{}/' CDKBridge"
-                command_str = command_str.format(self.py4py4j_jar_path, self.cdk_jar_path)
-                subprocess.Popen([command_str], shell=True, stdout=subprocess.PIPE)
+                print("Compiling CDKBridge")
+                command_str = "javac -cp {}:{}/ /mnt/code/modSAR/java/cdk_bridge.java"
+                command_str = command_str.format(self.py4j_jar_path, self.cdk_jar_path)
+                output = subprocess.run(command_str.split(), check=False, stdout=subprocess.PIPE)
+
+                print("Starting CDKBridge")
+                command_str = "java -cp {}:{}/:/mnt/code/modSAR/java/ CDKBridge &"
+                command_str = command_str.format(self.py4j_jar_path, self.cdk_jar_path)
+                subprocess.Popen(command_str.split(), stdout=subprocess.PIPE)
                 self.is_server_running = True
                 self.gateway = JavaGateway(gateway_parameters=GatewayParameters(auto_convert=True))
 
